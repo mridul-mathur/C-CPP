61eb65006acf20502b88fb22ab8c43012a1c287e
Mridulish_26 <91665131+mridul-mathur@users.noreply.github.com>
2023-09-11 18:31:08 +0000

    d1

---
+++ basic.c
@@ -0,0 +1,35 @@
+// c lang is a procedural language
+// static language
+
+#include <stdio.h>
+
+int main() { //int is the return type of the function
+    char i = 'S';
+    int j = 20;
+    float k = 15.05;
+    printf("Sakshi %f Mridul %d Pawan %c \n",j,i,k);
+    return 0;   // return 0 means the program ran successfully
+}
+/*
+1. keywords
+2. identifires
+3. constants & variables
+4. strings
+5. special symbols
+6. operators
+*/
+
+// variables are names given to memory locations that stores data.
+// int, float, double, char, void
+
+// int i;// declaration
+// float f;
+// double d;
+// char c;
+// void v;
+
+// i = 10; // initialization
+// f = 10.23;
+// d = 10.23456789;
+// c = 'a';
+// constants are fixed values that do not change during the execution of a program.
